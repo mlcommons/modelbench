2addadd27ce28c7652065a706eae1c18dcd4fd73
Brian Goldman <auxy@google.com>
2024-01-22 15:33:30 -0700

    Function for normalizing scores based on Peter's recommendation.
    This allows us to specify what a 0, 3, and 5 star raw value would be, while maintaining monotonicity. I went down a rabbit hole trying to use quadratic or cubic functions, but wasn't able to get monotonicity to work.

--- src/coffee/benchmark.py
+++ src/coffee/benchmark.py
@@ -1,10 +1,32 @@
 from abc import ABC, abstractmethod
-from typing import List
+from typing import List, Tuple
 
 import casefy
 
 from coffee.helm import BbqHelmTest, HelmTest, RealToxicityPromptsHelmTest
 
+
+def _linear_coefficients(p1, p2) -> Tuple[float, float]:
+    slope = (p2[1] - p1[1]) / (p2[0] - p1[0])
+    offset = p1[1] - slope * p1[0]
+    return slope, offset
+
+
+def piecewise_normalization(min_possible: float, mid_point: float, max_possible: float):
+    """Create a function will will normalize a float to the range [0-5].
+
+    The function is linear between [(min_possible, 0), (mid_point, 3)] and [(mid_point, 3), (max_possible, 5)].
+    """
+    below_mid = _linear_coefficients((min_possible, 0), (mid_point, 3))
+    above_mid = _linear_coefficients((mid_point, 3), (max_possible, 5))
+
+    def normalizer(score: float) -> float:
+        if score < mid_point:
+            return below_mid[0] * score + below_mid[1]
+        else:
+            return above_mid[0] * score + above_mid[1]
+
+    return normalizer
 
 class Benchmark(ABC):
     def __init__(self, sut, scores):
--- tests/test_benchmark.py
+++ tests/test_benchmark.py
@@ -1,5 +1,7 @@
 from coffee.helm import HelmSut, BbqHelmTest
-from coffee.benchmark import MakeshiftBiasBenchmark
+from coffee.benchmark import MakeshiftBiasBenchmark, piecewise_normalization
+import pytest
+import numpy as np
 
 
 def test_class_basics():
@@ -11,6 +13,35 @@ def test_class_basics():
 def test_instance_basics():
     rb = MakeshiftBiasBenchmark(HelmSut.GPT2, {})
     assert rb.sut == HelmSut.GPT2
+
+
+@pytest.mark.parametrize(
+    "min_possible,mid_point,max_possible",
+    [
+        (0, 3, 5),
+        (-10, 0, 10),
+        (-17, -5, 0),
+        (0, 99, 100),
+    ],
+)
+def test_piecewise_normalization(min_possible, mid_point, max_possible):
+    normalizer_fn = piecewise_normalization(min_possible, mid_point, max_possible)
+
+    assert normalizer_fn(min_possible) == 0
+    assert normalizer_fn(mid_point) == 3
+    assert normalizer_fn(max_possible) == 5
+
+    lower_half = (mid_point + min_possible) / 2
+    assert 0 <= normalizer_fn(lower_half) <= 3
+    upper_half = (max_possible + mid_point) / 2
+    assert 3 <= normalizer_fn(upper_half) <= 5
+
+    # Sample 20 points along the function to ensure they are monotonic.
+    xs = np.linspace(min_possible, max_possible, num=20)
+    for i in range(len(xs) - 1):
+        lower = normalizer_fn(xs[i])
+        higher = normalizer_fn(xs[i + 1])
+        assert lower < higher, f"Expected f({xs[i]}) to be less than f({xs[i+1]})"
 
 
 # not testing bechmark scoring for the moment because it's all throwaway

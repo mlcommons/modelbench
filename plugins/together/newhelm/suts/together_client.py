96464b3aa7e1769f1ca1ec817b51fd8618effc50
Brian W. Goldman <2237679+brianwgoldman@users.noreply.github.com>
2024-02-01 14:54:51 -0700

    Implement the inference endpoint for Together. (#84)
    After building it I decided LlamaGuard doesn't actually need it, but I figured it would be good to have around.

--- plugins/together/newhelm/suts/together_client.py
+++ plugins/together/newhelm/suts/together_client.py
@@ -1,5 +1,5 @@
-from typing import List, Optional
-from pydantic import BaseModel
+from typing import Any, List, Optional
+from pydantic import BaseModel, Field
 import requests
 from together.utils import response_status_exception  # type: ignore
 from newhelm.placeholders import Prompt
@@ -163,6 +163,100 @@ class TogetherChatSUT(PromptResponseSUT[TogetherChatRequest, TogetherChatRespons
             text = choice.message.content
             assert text is not None
             sut_completions.append(SUTCompletion(text))
+        return SUTResponse(sut_completions)
+
+
+class TogetherInferenceRequest(BaseModel):
+    # https://docs.together.ai/reference/inference
+    model: str
+    # prompt is documented as required, but you can pass messages instead,
+    # which is not documented.
+    prompt: Optional[str] = None
+    messages: Optional[List[TogetherChatRequest.Message]] = None
+    max_tokens: int
+    stop: Optional[List[str]] = None
+    temperature: Optional[float] = None
+    top_p: Optional[float] = None
+    top_k: Optional[int] = None
+    repetition_penalty: Optional[float] = None
+    safety_model: Optional[str] = None
+    n: Optional[int] = None
+
+
+class TogetherInferenceResponse(BaseModel):
+    class Args(BaseModel):
+        model: str
+        prompt: Optional[str] = None
+        temperature: float
+        top_p: float
+        top_k: float
+        max_tokens: int
+
+    status: str
+    prompt: List[str]
+    model: str
+    # Pydantic uses "model_" as the prefix for its methods, so renaming
+    # here to get out of the way.
+    owner: str = Field(alias="model_owner")
+    tags: Optional[Any] = None
+    num_returns: int
+    args: Args
+    subjobs: List
+
+    class Output(BaseModel):
+        class Choice(BaseModel):
+            finish_reason: str
+            index: Optional[int] = None
+            text: str
+
+        choices: List[Choice]
+        raw_compute_time: Optional[float] = None
+        result_type: str
+
+    output: Output
+
+
+class TogetherInferenceSUT(
+    PromptResponseSUT[TogetherInferenceRequest, TogetherInferenceResponse]
+):
+    _URL = "https://api.together.xyz/inference"
+
+    def __init__(self, model):
+        self.model = model
+
+    def translate_request(self, prompt: Prompt) -> TogetherInferenceRequest:
+        return TogetherInferenceRequest(
+            model=self.model,
+            prompt=prompt.text,
+            # TODO: Add messages here once issue #56 is resolved.
+            max_tokens=prompt.options.max_tokens,
+            stop=prompt.options.stop_sequences,
+            temperature=prompt.options.temperature,
+            top_p=prompt.options.top_p,
+            top_k=prompt.options.top_k_per_token,
+            repetition_penalty=prompt.options.frequency_penalty,
+            n=prompt.options.num_completions,
+        )
+
+    def evaluate(self, request: TogetherInferenceRequest) -> TogetherInferenceResponse:
+        api_key = SECRETS.get_required("together", "api_key")
+        headers = {
+            "Authorization": f"Bearer {api_key}",
+        }
+        as_json = request.model_dump(exclude_none=True)
+        response = requests.post(self._URL, headers=headers, json=as_json)
+        response_status_exception(response)
+        return TogetherInferenceResponse(**response.json())
+
+    def translate_response(
+        self, prompt: Prompt, response: TogetherInferenceResponse
+    ) -> SUTResponse:
+        for p in response.prompt:
+            print(p)
+        sut_completions = []
+        for choice in response.output.choices:
+            assert choice.text is not None
+            sut_completions.append(SUTCompletion(choice.text))
         return SUTResponse(sut_completions)
 
 

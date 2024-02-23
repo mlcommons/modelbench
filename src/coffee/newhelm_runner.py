1efe3abfe4fcf3aa67e6f9d556bdb85149667d03
William Pietri <william-github.com@scissor.com>
2024-02-23 10:02:55 -0800

    Switch reference SUT from GPT2 to Pythia 70m. Slightly expand number of items used for calibration run. (#114)

--- src/coffee/newhelm_runner.py
+++ src/coffee/newhelm_runner.py
@@ -1,6 +1,9 @@
 import dataclasses
 from enum import Enum
 
+from newhelm.sut_registry import SUTS
+from newhelm.suts.huggingface_client import HuggingFaceSUT
+
 
 @dataclasses.dataclass
 class SutDescription:
@@ -11,6 +14,10 @@ class SutDescription:
         return super().__hash__()
 
 
+SUTS.register("pythia-70m", HuggingFaceSUT, "EleutherAI/pythia-70m")
+
+
 class NewhelmSut(SutDescription, Enum):
     GPT2 = "gpt2", "OpenAI GPT-2"
     LLAMA_2_7B = "llama-2-7b", "Meta Llama 2, 7b parameters"
+    PYTHIA_70M = "pythia-70m", "EleutherAI Pythia, 70m parameters"

490c1e71f9d0bebac27f0385b34112ec8fdbe657
Brian W. Goldman <2237679+brianwgoldman@users.noreply.github.com>
2024-02-27 16:21:11 -0700

    Add test coverage for run-sut command. (#209)
    Also remove combinatorics from run-test.

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -23,13 +23,25 @@ def test_list_plugins(cmd):
 
 @expensive_tests
 @pytest.mark.parametrize("sut", ["demo_yes_no"])
+def test_run_sut_demos(cmd, sut):
+    assert (
+        os.system(
+            f"""python {cmd} run-sut \
+                --sut {sut} \
+                --prompt "Can you say Hello?" """
+        )
+        == 0
+    )
+
+
+@expensive_tests
 @pytest.mark.parametrize("test", ["demo_01", "demo_02", "demo_03", "demo_04"])
-def test_test_sut_combinations(cmd, test, sut):
+def test_run_test_demos(cmd, test):
     assert (
         os.system(
             f"""python {cmd} run-test \
                 --test {test} \
-                --sut {sut} \
+                --sut demo_yes_no \
                 --max-test-items 1"""
         )
         == 0

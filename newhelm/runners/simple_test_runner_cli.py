adfd00ae18261a12e0445e7eed1db1e71b567e5e
Brian W. Goldman <2237679+brianwgoldman@users.noreply.github.com>
2024-02-28 09:59:36 -0700

    Have run-test write a json file, and dump less to the screen. (#208)

--- newhelm/runners/simple_test_runner_cli.py
+++ newhelm/runners/simple_test_runner_cli.py
@@ -1,3 +1,5 @@
+import os
+from typing import Optional
 import click
 from newhelm.base_test import BasePromptResponseTest
 from newhelm.command_line import (
@@ -24,6 +26,10 @@ from newhelm.test_registry import TESTS
 @SECRETS_FILE_OPTION
 @MAX_TEST_ITEMS_OPTION
 @click.option(
+    "--output-file",
+    help="If specified, will override the default location for outputting the TestRecord.",
+)
+@click.option(
     "--no-caching",
     is_flag=True,
     show_default=True,
@@ -36,6 +42,7 @@ def run_test(
     data_dir: str,
     secrets: str,
     max_test_items: int,
+    output_file: Optional[str],
     no_caching: bool,
 ):
     """Run the Test on the desired SUT and output the TestRecord."""
@@ -46,7 +53,10 @@ def run_test(
     assert isinstance(sut_obj, PromptResponseSUT)
     assert isinstance(test_obj, BasePromptResponseTest)
 
-    test_journal = run_prompt_response_test(
+    if output_file is None:
+        os.makedirs("output", exist_ok=True)
+        output_file = os.path.join("output", f"record_for_{test}_{sut}.json")
+    test_record = run_prompt_response_test(
         test,
         test_obj,
         sut,
@@ -55,4 +65,8 @@ def run_test(
         max_test_items,
         use_caching=not no_caching,
     )
-    print(test_journal.model_dump_json(indent=4))
+    with open(output_file, "w") as f:
+        print(test_record.model_dump_json(indent=4), file=f)
+    # For displaying to the screen, clear out the verbose test_item_records
+    test_record.test_item_records = []
+    print(test_record.model_dump_json(indent=4))

c97a349c0344bd07513f229de7b38f29f1843bbe
Brian W. Goldman <2237679+brianwgoldman@users.noreply.github.com>
2024-02-28 17:06:58 -0700

    Parameterize test_object_creation to make failures more traceable. (#218)

--- plugins/validation_tests/test_object_creation.py
+++ plugins/validation_tests/test_object_creation.py
@@ -1,3 +1,4 @@
+import pytest
 from newhelm.load_plugins import load_plugins
 from newhelm.record_init import get_initialization_record
 from newhelm.sut_registry import SUTS
@@ -7,15 +8,15 @@ from newhelm.test_registry import TESTS
 load_plugins()
 
 
-def test_all_tests_construct_and_record_init():
-    for _, entry in TESTS.items():
-        test = entry.make_instance()
-        # This throws if things are set up incorrectly.
-        get_initialization_record(test)
+@pytest.mark.parametrize("test_name", [key for key, _ in TESTS.items()])
+def test_all_tests_construct_and_record_init(test_name):
+    test = TESTS.make_instance(test_name)
+    # This throws if things are set up incorrectly.
+    get_initialization_record(test)
 
 
-def test_all_suts_construct_and_record_init():
-    for _, entry in SUTS.items():
-        sut = entry.make_instance()
-        # This throws if things are set up incorrectly.
-        get_initialization_record(sut)
+@pytest.mark.parametrize("sut_name", [key for key, _ in SUTS.items()])
+def test_all_suts_construct_and_record_init(sut_name):
+    sut = SUTS.make_instance(sut_name)
+    # This throws if things are set up incorrectly.
+    get_initialization_record(sut)

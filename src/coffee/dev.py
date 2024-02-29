4d9b9c1c6b17838b568f127b1bafe2743d3bb58a
Mike Lay <mike@mkly.io>
2024-02-29 13:45:03 -0800

    Add dev build command for local development
    * Add dev build command for local development
    
    This is to allow for a more rapid iteration loop than running a full sut
    benchmark.
    
    This is preliminary data for benchmark scores, but should become more
    full featured in the future.
    
    Intended to supersede `--web-only` option in run benchmark cli

---
+++ src/coffee/dev.py
@@ -0,0 +1,55 @@
+import click
+import pathlib
+import pickle
+
+from coffee.static_site_generator import StaticSiteGenerator
+
+from coffee.newhelm_runner import NewhelmSut
+from coffee.benchmark import (
+    GeneralChatBotBenchmarkDefinition,
+    BiasHazardDefinition,
+    HazardScore,
+    BenchmarkScore,
+    ToxicityHazardDefinition,
+)
+
+
+def benchmark_scores():
+    bd = GeneralChatBotBenchmarkDefinition()
+    bs = [
+        BenchmarkScore(
+            bd,
+            NewhelmSut.GPT2,
+            [
+                HazardScore(BiasHazardDefinition(), 0.5),
+                HazardScore(ToxicityHazardDefinition(), 0.8),
+            ],
+        ),
+        BenchmarkScore(
+            bd,
+            NewhelmSut.LLAMA_2_7B,
+            [
+                HazardScore(BiasHazardDefinition(), 0.5),
+                HazardScore(ToxicityHazardDefinition(), 0.8),
+            ],
+        ),
+    ]
+    return bs
+
+
+@click.group()
+def cli() -> None:
+    pass
+
+
+@cli.command(help="Generate a simple site for development")
+@click.option(
+    "--output-dir", "-o", default="./web", type=click.Path(file_okay=False, dir_okay=True, path_type=pathlib.Path)
+)
+def build(output_dir: pathlib.Path) -> None:
+    generator = StaticSiteGenerator()
+    generator.generate(benchmark_scores(), output_dir)
+
+
+if __name__ == "__main__":
+    cli()

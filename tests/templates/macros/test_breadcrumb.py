56980c6fe9358e761e4bb6d9f932b9295dcb166f
dhosterman <daniel@danielhosterman.com>
2024-03-04 16:29:43 -0500

    Initial move toward using toml content files to populate web templates.

--- tests/templates/macros/test_breadcrumb.py
+++ tests/templates/macros/test_breadcrumb.py
@@ -2,9 +2,9 @@ def test_display_breadcrumb(benchmark_score, template_env):
     template = template_env.get_template("macros/breadcrumb.html")
     result = template.module.breadcrumb(benchmark_score, benchmark_score.benchmark_definition)
     assert "Benchmarks" in result
-    assert "General Chat Bot" in result
+    assert "Benchmark for general purpose AI chat model" in result
     assert 'href="general_chat_bot_benchmark.html"' in result
     result = template.module.breadcrumb(None, benchmark_score.benchmark_definition)
     assert "Benchmarks" in result
     assert 'href="general_chat_bot_benchmark.html"' not in result
-    assert "General Chat Bot" in result
+    assert "Benchmark for general purpose AI chat model" in result
--- tests/templates/test_test_report.py
+++ tests/templates/test_test_report.py
@@ -1,12 +1,11 @@
-def test_test_report(benchmark_score, template_env, stars_description):
+def test_test_report(benchmark_score, template_env):
     template = template_env.get_template("test_report.html")
     result = template.render(
         benchmark_score=benchmark_score,
-        stars_description=stars_description,
     )
     assert "Test Report" in result
     assert "66% safe responses" in result
     assert "Tests Run" in result
     assert "Test Details" in result
-    assert "General Chat Bot" in result
+    assert "Benchmark for general purpose AI chat model" in result
     assert "Don't see the tests you are looking for?" in result

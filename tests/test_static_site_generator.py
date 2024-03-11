c35a0d45364ee196d315da759210be67cd941f77
Mike Lay <mike@mkly.io>
2024-03-11 15:00:37 -0700

    Add additional embed logic for relative links
    * Add additional embed logic for relative links
    * Account for level(via `page type`) and embed status (via
      `view_embed`).
    
    This is because we need a root node to branch off of. For example the
    site can have a page at `/example/benchmarks` and we need to intercept a
    page call to add the other pages at
    `/example/benchmarks/some_test_result`

--- tests/test_static_site_generator.py
+++ tests/test_static_site_generator.py
@@ -99,14 +99,18 @@ def test_root_path(static_site_generator, static_site_generator_view_embed):
 
 
 def test_benchmarks_path(static_site_generator, static_site_generator_view_embed):
-    assert static_site_generator.benchmarks_path() == "benchmarks.html"
-    assert static_site_generator_view_embed.benchmarks_path() == "../benchmarks"
+    assert static_site_generator.benchmarks_path(page_type="benchmarks") == "benchmarks.html"
+    assert static_site_generator_view_embed.benchmarks_path(page_type="benchmarks") == "../benchmarks"
+    assert static_site_generator_view_embed.benchmarks_path(page_type="benchmark") == "../../benchmarks"
 
 
 def test_benchmark_path(static_site_generator, static_site_generator_view_embed):
-    assert static_site_generator.benchmark_path("general_chat_bot_benchmark") == "general_chat_bot_benchmark.html"
+    assert static_site_generator.benchmark_path("general_chat_bot_benchmark", page_type="benchmarks") == "general_chat_bot_benchmark.html"
     assert (
-        static_site_generator_view_embed.benchmark_path("general_chat_bot_benchmark") == "../general_chat_bot_benchmark"
+        static_site_generator_view_embed.benchmark_path("general_chat_bot_benchmark", page_type="test_report") == "../general_chat_bot_benchmark"
+    )
+    assert (
+        static_site_generator_view_embed.benchmark_path("general_chat_bot_benchmark", page_type="benchmarks") == "general_chat_bot_benchmark"
     )
 
 

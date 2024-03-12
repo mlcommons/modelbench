d991e9c5ee1e2b478ba3eb420f3fdadb9d624459
Daniel Hosterman <daniel@danielhosterman.com>
2024-03-12 14:07:14 -0400

    discover-content-from-templates (#179)
    * A possibly terrible way to dynamically determine the content the html templates are expecting and ensure that the content exists for all of the possible benchmarks, hazards, and suts.
    
    * Refactor and add some documentation so the code is more understandable.
    
    * Add a little more documentation.

--- tests/test_static_site_generator.py
+++ tests/test_static_site_generator.py
@@ -1,5 +1,6 @@
-import itertools
 import pathlib
+from unittest.mock import MagicMock
+from unittest.mock import patch
 
 SIMPLE_BBQ_DATA = pathlib.Path(__file__).parent / "data/full_runs/simple_bbq"
 
@@ -119,32 +120,87 @@ class TestObjectContentKeysExist:
         _ssg = StaticSiteGenerator()
         return _ssg
 
-    @pytest.mark.parametrize(
-        "subclass, key",
-        itertools.product(
-            BenchmarkDefinition.__subclasses__(),
-            ["name", "tagline", "application", "hazards_description", "limitations"],
-        ),
-    )
-    def test_benchmark_definitions(self, ssg, subclass, key):
-        assert ssg.content(subclass(), key)
+    @pytest.fixture
+    def benchmark_score(self):
+        bd = GeneralChatBotBenchmarkDefinition()
+        bh = BiasHazardDefinition()
+        bs = BenchmarkScore(
+            bd,
+            NewhelmSut.GPT2,
+            [
+                HazardScore(bh, bh.three_star_standard(), None, None),
+            ],
+            None,
+            None,
+        )
+        return bs
+
+    @pytest.fixture
+    def mock_content(self, benchmark_score):
+        """
+        Mock the content method of StaticSiteGenerator and render each template with enough arguments to ensure
+        all the partials, iterators, etc. are being exercised. Return the mocked content object so we can introspect its
+        call args.
+        """
+        with patch("coffee.static_site_generator.StaticSiteGenerator.content") as mock_content:
+            _ssg = StaticSiteGenerator()
+
+            # We need to mock the undefined method of Environment because templates can ask for arbitrary things
+            # and by returning a MagicMock, we can both satisfy any requirements on those objects as well as
+            # giving us a return value we can introspect if we need to later on.
+            def undefined(obj=None, name=None):
+                return MagicMock(name=name)
+
+            _ssg.env.undefined = undefined
+
+            for template in (pathlib.Path(__file__).parent.parent / "src" / "coffee" / "templates").glob("*.html"):
+                _ssg._render_template(
+                    template.name,
+                    benchmark_score=benchmark_score,
+                    benchmark_definition=benchmark_score.benchmark_definition,
+                    grouped_benchmark_scores=_ssg._grouped_benchmark_scores([benchmark_score]),
+                )
+            return mock_content
+
+    @pytest.fixture(autouse=True)
+    def required_template_content_keys(self, mock_content):
+        """
+        Return a dict with all keys requested from the calls to the mocked content object passed in. These keys are
+        either a string ("general") or the name of a base class for a Sut, Benchmark, Harm, etc. These can be used
+        in assertions in tests.
+        """
+        required_keys = dict()
+        for call in mock_content.call_args_list:
+            if isinstance(call.args[0], str):
+                key = call.args[0]
+            else:
+                key = call.args[0].__class__.__base__.__name__
+            if key not in required_keys:
+                required_keys[key] = set()
+            required_keys[key].add(call.args[1])
+
+        return required_keys
 
     @pytest.mark.parametrize(
-        "subclass, key",
-        itertools.product(
-            HazardDefinition.__subclasses__(),
-            ["name", "description"],
-        ),
+        "benchmark",
+        [subclass() for subclass in BenchmarkDefinition.__subclasses__()],
     )
-    def test_hazard_definitions(self, ssg, subclass, key):
-        assert ssg.content(subclass(), key)
+    def test_benchmark_definitions(self, ssg, benchmark, required_template_content_keys):
+        for key in required_template_content_keys["BenchmarkDefinition"]:
+            assert ssg.content(benchmark, key)
 
     @pytest.mark.parametrize(
-        "item, key",
-        itertools.product(
-            NewhelmSut,
-            ["name", "tagline"],
-        ),
+        "sut_key",
+        NewhelmSut,
     )
-    def test_suts(self, ssg, item, key):
-        assert ssg.content(item, key)
+    def test_sut_definitions(self, ssg, sut_key, required_template_content_keys):
+        for key in required_template_content_keys["SutDescription"]:
+            assert ssg.content(sut_key, key)
+
+    @pytest.mark.parametrize(
+        "hazard",
+        [subclass() for subclass in HazardDefinition.__subclasses__()],
+    )
+    def test_hazard_definitions(self, ssg, hazard, required_template_content_keys):
+        for key in required_template_content_keys["HazardDefinition"]:
+            assert ssg.content(hazard, key)

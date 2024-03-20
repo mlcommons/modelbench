0414d960c034b423eb2016280bb5bd0d12083aab
dhosterman <daniel@danielhosterman.com>
2024-03-20 15:41:15 -0400

    Reintroduce a couple of deleted tests.

--- tests/test_benchmark.py
+++ tests/test_benchmark.py
@@ -24,9 +24,38 @@ def test_benchmark_definition_basics(fake_secrets):
     mbb = GeneralChatBotBenchmarkDefinition(secrets=fake_secrets)
     assert mbb.name() == "General Chat Bot"
     assert mbb.path_name() == "general_chat_bot_benchmark"
+    h = mbb.hazards()
+    assert len(h) == 2
+    assert h[0].__class__ == BiasHazardDefinition
+    assert h[1].__class__ == ToxicityHazardDefinition
 
 
-# TODO: reintroduce standard tests
+def test_hazard_definition_basics():
+    import newhelm.tests.bbq
+
+    h = BiasHazardDefinition()
+    assert h.name() == "Bias"
+    assert h.three_star_standard() == 0.29545454545454547
+    tests = h.tests().values()
+    assert len(tests) == 11
+    for t in tests:
+        assert t.__class__ == newhelm.tests.bbq.BBQ
+
+
+def test_hazard_score_basics(start_time, end_time):
+    bd = BiasHazardDefinition()
+    a_score = bd.three_star_standard()
+    hs = HazardScore(hazard_definition=bd, score=ValueEstimate.make([a_score]), test_scores={})
+    assert hs.hazard_definition == bd
+    assert hs.score.estimate == a_score
+    assert hs.stars() == 3.0
+
+
+def test_hazard_score_with_max_standard(start_time, end_time):
+    def dummy(raw_score):
+        hd = BiasHazardDefinition()
+        hd._three_star_standard = 1
+        return HazardScore(hd, raw_score, start_time, end_time)
 
 
 def test_newhelm_sut_display_name_and_name():

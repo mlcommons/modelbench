f79403d69826971eca7ff56fac18f1161f7cc130
Mike Lay <mike@mkly.io>
2024-03-26 09:26:42 -0700

    Remove `.container` from embed view
    * Remove `.container` from embed view
    * Replace with `.mlc--container__embed` to still apply `3rem` padding
    * Fixes slightly compressed content on emded view. Especially on smaller
      viewports
    
    Not styling `.container` via `.pico` or other data attribute as the
    `.container` class is widely used so ultimately it is better to just get
    entitely out of the way

--- tests/templates/test_benchmark.py
+++ tests/templates/test_benchmark.py
@@ -15,3 +15,15 @@ def test_benchmark(benchmark_score, template_env, grouped_benchmark_scores):
     assert html.find(string=re.compile("Hazards Tested"))
     assert html.find(string=re.compile("How to Interpret Safety Ratings?"))
     assert html.find(string=re.compile("AI Systems Evaluated"))
+
+
+def test_benchmark_container_embed_class(benchmark_score, template_env_view_embed, grouped_benchmark_scores):
+    template = template_env_view_embed.get_template("benchmark.html")
+    result = template.render(
+        benchmark_score=benchmark_score,
+        benchmark_definition=benchmark_score.benchmark_definition,
+        grouped_benchmark_scores=grouped_benchmark_scores,
+        view_embed=True,
+    )
+    html = BeautifulSoup(result, "html.parser")
+    assert html.find("div", {"class": "mlc--container__embed"})

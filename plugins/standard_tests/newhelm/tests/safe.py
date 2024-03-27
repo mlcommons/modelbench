0fc476b1e782c2b435a2934420a39746114ac707
Barbara Korycki <barbara@mlcommons.org>
2024-03-27 17:26:35 -0400

    Add new tests for 22mar2024 release (#306)
    * Add new tests for 22mar2024 release
    
    * Update hazard taxonomy to Meta version
    
    * Re-structure result to a persona-mapping
    
    * Minor cleanup
    
    * SafeTestResult now contains a mapping from personas to PersonaResults

--- plugins/standard_tests/newhelm/tests/safe.py
+++ plugins/standard_tests/newhelm/tests/safe.py
@@ -6,9 +6,9 @@ from newhelm.annotators.llama_guard_annotator import (
     LlamaGuardAnnotator,
 )
 from pydantic import BaseModel
-from newhelm.aggregations import mean_of_measurement
+from newhelm.aggregations import get_measurement_stats_by_key
 from newhelm.base_annotator import BaseAnnotator
-from newhelm.base_test import BasePromptResponseTest, TestMetadata
+from newhelm.base_test import BasePromptResponseTest
 from newhelm.dependency_helper import DependencyHelper
 from newhelm.external_data import ExternalData, GDriveData
 from newhelm.prompt import TextPrompt, SUTOptions
@@ -26,16 +26,29 @@ from newhelm.test_registry import TESTS
 
 
 HAZARDS_MAPPING = {
-    "cae": "Child abuse and exploitation",
+    "cae": "Child sexual exploitation",
     "cbr": "Chemical, biological, radioloigcal, nuclear and high-yield explosives weapons",
-    "gra": "Graphic depictions of hazards",
-    "ssh": "Suicide, self-harm and eating disorders",
+    "hat": "Hate",
+    "nvc": "Non-violent crimes",
+    "sc": "Sex-related crimes",
+    "ssh": "Suicide & self-harm",
     "ter": "Terrorism",
+    "vio": "Violent crimes",
 }
+_PERSONA_TYPES = ["typical", "malicious", "vulnerable"]
+
+
+class SafeTestItemContext(BaseModel):
+    persona_type: str
+
+
+class PersonaResult(BaseModel):
+    frac_safe: float
+    num_items: int
 
 
 class SafeTestResult(BaseModel):
-    frac_safe: float
+    persona_results: Mapping[str, PersonaResult]
 
 
 @newhelm_test(requires_sut_capabilities=[AcceptsTextPrompt])
@@ -46,24 +59,35 @@ class SafeTest(BasePromptResponseTest):
         hazard: str,
         folder_name: str,
         together_api_key: TogetherApiKey,
-        benign_prompts: bool = False,
+        persona_types: List[str],
     ):
         super().__init__(uid)
         assert hazard in HAZARDS_MAPPING, f"Invalid hazard type {hazard}."
+        invalid_personas = [
+            persona for persona in persona_types if persona not in _PERSONA_TYPES
+        ]
+        assert len(invalid_personas) == 0, f"Invalid persona types {invalid_personas}."
+        assert len(persona_types), "At least one persona type must be specified."
+        assert len(persona_types) == len(
+            set(persona_types)
+        ), "Persona types may not contain duplicates."
         self.hazard = hazard
         self.hazard_name = HAZARDS_MAPPING[hazard]
         self.folder_name = folder_name
         self.together_api_key = together_api_key
-        self.prompt_type = "hazardous" if not benign_prompts else "benign"
-        self.dependency_key = f"{self.folder_name}_{self.hazard}_{self.prompt_type}"
+        self.persona_types = persona_types
+
+    def _persona_dependency_key(self, persona: str) -> str:
+        return f"{self.folder_name}_{self.hazard}_{persona}"
 
     def get_dependencies(self) -> Mapping[str, ExternalData]:
-        return {
-            f"{self.dependency_key}": GDriveData(
-                data_source="https://drive.google.com/drive/folders/1FNpd9KQ1MvylgVNuWx7O0byWoFDyLBeS",
-                file_path=f"{self.folder_name}/mls_ais_wg_safe_{self.hazard}_{self.prompt_type}.csv",
-            ),
-        }
+        dependencies = {}
+        for persona in self.persona_types:
+            dependencies[self._persona_dependency_key(persona)] = GDriveData(
+                data_source="https:/drive.google.com/drive/folders/1FNpd9KQ1MvylgVNuWx7O0byWoFDyLBeS",
+                file_path=f"{self.folder_name}/mls_ais_wg_safe_{self.hazard}_{persona}-user.csv",
+            )
+        return dependencies
 
     def make_test_items(self, dependency_helper: DependencyHelper) -> List[TestItem]:
         sut_options = SUTOptions(
@@ -71,22 +95,30 @@ class SafeTest(BasePromptResponseTest):
             temperature=0.01,
         )
         test_items: List[TestItem] = []
-        data_file = dependency_helper.get_local_path(self.dependency_key)
-        with open(data_file, "r") as csvfile:
-            reader = csv.DictReader(csvfile)
-            for row in reader:
-                text = row["Prompt"].strip()
-                if not text:
-                    continue
-                file_hazard = row["Hazard type shorthand"]
-                assert (
-                    file_hazard == self.hazard
-                ), f"Expected {data_file} to have {self.hazard}, but had {file_hazard}."
-                prompt = PromptWithContext(
-                    prompt=TextPrompt(text=text, options=sut_options),
-                    source_id=row["UID"],
-                )
-                test_items.append(TestItem(prompts=[prompt]))
+        for persona in self.persona_types:
+            data_file = dependency_helper.get_local_path(
+                self._persona_dependency_key(persona)
+            )
+            with open(data_file, "r") as csvfile:
+                reader = csv.DictReader(csvfile)
+                for row in reader:
+                    text = row["Prompt"].strip()
+                    if not text:
+                        continue
+                    file_hazard = row["Hazard type shorthand"]
+                    assert (
+                        file_hazard == self.hazard
+                    ), f"Expected {data_file} to have {self.hazard}, but had {file_hazard}."
+                    prompt = PromptWithContext(
+                        prompt=TextPrompt(text=text, options=sut_options),
+                        source_id=row["UID"],
+                    )
+                    test_items.append(
+                        TestItem(
+                            prompts=[prompt],
+                            context=SafeTestItemContext(persona_type=persona),
+                        ),
+                    )
         return test_items
 
     def get_annotators(self) -> Mapping[str, BaseAnnotator]:
@@ -104,32 +136,27 @@ class SafeTest(BasePromptResponseTest):
         """
         Returns the fraction [0.0, 1.0] of responses that were evaluated as safe.
         """
-        return SafeTestResult(frac_safe=mean_of_measurement("is_safe", items))
+        measurement_stats = get_measurement_stats_by_key(
+            "is_safe", items, key=lambda item: item.test_item.context.persona_type
+        )
+        persona_results = {}
+        for persona, persona_measurements in measurement_stats.items():
+            persona_results[persona] = PersonaResult(
+                frac_safe=persona_measurements.mean,
+                num_items=persona_measurements.count,
+            )
+        return SafeTestResult(persona_results=persona_results)
 
 
-FOLDER_NAME = "data_08mar2024"
+FOLDER_NAME = "data_22mar2024"
 API_KEY_SECRET = InjectSecret(TogetherApiKey)
-# Hazardous Prompts
 TESTS.register(
     SafeTest,
     "safe-cae",
     "cae",
     FOLDER_NAME,
     API_KEY_SECRET,
-)
-TESTS.register(
-    SafeTest,
-    "safe-ssh",
-    "ssh",
-    FOLDER_NAME,
-    API_KEY_SECRET,
-)
-TESTS.register(
-    SafeTest,
-    "safe-ter",
-    "ter",
-    FOLDER_NAME,
-    API_KEY_SECRET,
+    persona_types=["typical", "malicious"],
 )
 TESTS.register(
     SafeTest,
@@ -137,36 +164,53 @@ TESTS.register(
     "cbr",
     FOLDER_NAME,
     API_KEY_SECRET,
+    persona_types=["malicious"],
 )
 TESTS.register(
     SafeTest,
-    "safe-gra",
-    "gra",
+    "safe-hat",
+    "hat",
     FOLDER_NAME,
     API_KEY_SECRET,
+    persona_types=["typical", "malicious"],
 )
-# Benign prompts
 TESTS.register(
     SafeTest,
-    "safe-cae-benign",
-    "cae",
+    "safe-nvc",
+    "nvc",
     FOLDER_NAME,
     API_KEY_SECRET,
-    benign_prompts=True,
+    persona_types=["typical", "malicious"],
 )
 TESTS.register(
     SafeTest,
-    "safe-ssh-benign",
+    "safe-sc",
+    "sc",
+    FOLDER_NAME,
+    API_KEY_SECRET,
+    persona_types=["typical", "malicious"],
+)
+TESTS.register(
+    SafeTest,
+    "safe-ssh",
     "ssh",
     FOLDER_NAME,
     API_KEY_SECRET,
-    benign_prompts=True,
+    persona_types=["typical", "vulnerable"],
 )
 TESTS.register(
     SafeTest,
-    "safe-ter-benign",
+    "safe-ter",
     "ter",
     FOLDER_NAME,
     API_KEY_SECRET,
-    benign_prompts=True,
+    persona_types=["typical", "malicious"],
+)
+TESTS.register(
+    SafeTest,
+    "safe-vio",
+    "vio",
+    FOLDER_NAME,
+    API_KEY_SECRET,
+    persona_types=["malicious"],
 )
--- plugins/standard_tests/tests/test_safe.py
+++ plugins/standard_tests/tests/test_safe.py
@@ -1,7 +1,15 @@
 import pytest
 
 from newhelm.prompt import TextPrompt
-from newhelm.tests.safe import SafeTest, HAZARDS_MAPPING, FOLDER_NAME, SafeTestResult
+from newhelm.tests.safe import (
+    SafeTest,
+    HAZARDS_MAPPING,
+    _PERSONA_TYPES,
+    FOLDER_NAME,
+    SafeTestItemContext,
+    PersonaResult,
+    SafeTestResult,
+)
 from newhelm.single_turn_prompt_response import (
     MeasuredTestItem,
     TestItem,
@@ -13,12 +21,16 @@ from tests.fake_dependency_helper import FakeDependencyHelper, make_csv
 HAZARDS_LIST = list(HAZARDS_MAPPING.keys())
 _HAZARD_1 = HAZARDS_LIST[0]
 _HAZARD_2 = HAZARDS_LIST[1]
+_PERSONA_1 = _PERSONA_TYPES[0]
+_PERSONA_2 = _PERSONA_TYPES[1]
 
 
-def _fake_dependency_helper(tmpdir, hazard, rows):
+def _fake_dependency_helper(tmpdir, hazard, persona_rows_dict):
     csv_header = ["Prompt", "Hazard type shorthand", "UID"]
-    csv_text = make_csv(csv_header, rows)
-    return FakeDependencyHelper(tmpdir, {f"{FOLDER_NAME}_{hazard}_hazardous": csv_text})
+    dependencies = {}
+    for persona, rows in persona_rows_dict.items():
+        dependencies[f"{FOLDER_NAME}_{hazard}_{persona}"] = make_csv(csv_header, rows)
+    return FakeDependencyHelper(tmpdir, dependencies)
 
 
 def test_make_test_items(tmpdir):
@@ -26,8 +38,16 @@ def test_make_test_items(tmpdir):
         ["prompt 1", _HAZARD_1, "id1"],
         ["prompt 2", _HAZARD_1, "id2"],
     ]
-    test = SafeTest("uid", _HAZARD_1, FOLDER_NAME, TogetherApiKey("some-value"))
-    dependency_helper = _fake_dependency_helper(tmpdir, _HAZARD_1, text_prompts)
+    test = SafeTest(
+        "uid",
+        _HAZARD_1,
+        FOLDER_NAME,
+        TogetherApiKey("some-value"),
+        persona_types=[_PERSONA_1],
+    )
+    dependency_helper = _fake_dependency_helper(
+        tmpdir, _HAZARD_1, {_PERSONA_1: text_prompts}
+    )
     test_items = test.make_test_items(dependency_helper)
 
     assert len(test_items) == 2
@@ -42,9 +62,15 @@ def test_make_test_items(tmpdir):
 
 def test_hazard_mismatch_exception(tmpdir):
     """An error is raised if the test encounters an item in the dataset with a different hazard."""
-    test = SafeTest("uid", _HAZARD_1, FOLDER_NAME, TogetherApiKey("some-value"))
+    test = SafeTest(
+        "uid",
+        _HAZARD_1,
+        FOLDER_NAME,
+        TogetherApiKey("some-value"),
+        persona_types=[_PERSONA_1],
+    )
     dependency_helper = _fake_dependency_helper(
-        tmpdir, _HAZARD_1, [["prompt", _HAZARD_2, "id"]]
+        tmpdir, _HAZARD_1, {_PERSONA_1: [["prompt", _HAZARD_2, "id"]]}
     )
     with pytest.raises(AssertionError) as err_info:
         test.make_test_items(dependency_helper)
@@ -56,22 +82,109 @@ def test_different_hazards(tmpdir):
     """Checks that all tests will produce identical TestItems for datasets that only differ in the hazard_name column."""
     items = []
     for hazard in HAZARDS_LIST:
-        test = SafeTest("uid", hazard, FOLDER_NAME, TogetherApiKey("some-value"))
+        test = SafeTest(
+            "uid",
+            hazard,
+            FOLDER_NAME,
+            TogetherApiKey("some-value"),
+            persona_types=[_PERSONA_1],
+        )
         dependency_helper = _fake_dependency_helper(
-            tmpdir, hazard, [["prompt", hazard, "id"]]
+            tmpdir, hazard, {_PERSONA_1: [["prompt", hazard, "id"]]}
         )
         items.append(test.make_test_items(dependency_helper)[0])
     assert all(item == items[0] for item in items)
 
 
+def test_different_persona_dependency_keys(tmpdir):
+    """Test uses correct dependency key mapping for each persona."""
+    dependencies = {}
+    for persona in _PERSONA_TYPES:
+        dependencies[persona] = [[f"{persona} prompt", _HAZARD_1, "id1"]]
+    dependency_helper = _fake_dependency_helper(tmpdir, _HAZARD_1, dependencies)
+
+    for persona in _PERSONA_TYPES:
+        test = SafeTest(
+            "uid",
+            _HAZARD_1,
+            FOLDER_NAME,
+            TogetherApiKey("some-value"),
+            persona_types=[persona],
+        )
+        item = test.make_test_items(dependency_helper)[0]
+        assert item.prompts[0].prompt.text == f"{persona} prompt"
+
+
+def test_multiple_personas_test_items(tmpdir):
+    prompts = ["prompt 1", "prompt 2", "prompt 3"]
+    dependency_helper = _fake_dependency_helper(
+        tmpdir,
+        _HAZARD_1,
+        {
+            _PERSONA_1: [[prompts[0], _HAZARD_1, "id1"]],
+            _PERSONA_2: [
+                [prompts[1], _HAZARD_1, "id2"],
+                [prompts[2], _HAZARD_1, "id3"],
+            ],
+        },
+    )
+    test = SafeTest(
+        "uid",
+        _HAZARD_1,
+        FOLDER_NAME,
+        TogetherApiKey("some-value"),
+        persona_types=[_PERSONA_1, _PERSONA_2],
+    )
+    items = test.make_test_items(dependency_helper)
+    assert [item.prompts[0].prompt.text for item in items] == prompts
+    assert [item.context.persona_type for item in items] == [
+        _PERSONA_1,
+        _PERSONA_2,
+        _PERSONA_2,
+    ]
+
+
 def test_unknown_hazard_exception():
     with pytest.raises(AssertionError, match="Invalid hazard type"):
-        SafeTest("uid", "New Hazard", FOLDER_NAME, TogetherApiKey("some-value"))
+        SafeTest(
+            "uid",
+            "New Hazard",
+            FOLDER_NAME,
+            TogetherApiKey("some-value"),
+            persona_types=[_PERSONA_1],
+        )
+
+
+def test_no_specified_personas_exception():
+    with pytest.raises(
+        AssertionError, match="At least one persona type must be specified"
+    ):
+        SafeTest(
+            "uid",
+            _HAZARD_1,
+            FOLDER_NAME,
+            TogetherApiKey("some-value"),
+            persona_types=[],
+        )
+
+
+def test_unknown_persona_exception():
+    with pytest.raises(AssertionError, match="Invalid persona types") as err_info:
+        SafeTest(
+            "uid",
+            _HAZARD_1,
+            FOLDER_NAME,
+            TogetherApiKey("some-value"),
+            persona_types=[_PERSONA_1, "new persona"],
+        )
+    assert _PERSONA_1 not in str(err_info.value)
+    assert "new persona" in str(err_info.value)
 
 
 def test_aggregate_measurements():
     dummy_item = TestItem(
-        prompts=[PromptWithContext(prompt=TextPrompt(text="prompt"), source_id="id")]
+        prompts=[PromptWithContext(prompt=TextPrompt(text="prompt"), source_id="id")],
+        context=SafeTestItemContext(persona_type=_PERSONA_1),
     )
     # 2 safe, 1 unsafe
     measured_test_items = [
@@ -79,6 +192,45 @@ def test_aggregate_measurements():
         MeasuredTestItem(test_item=dummy_item, measurements={"is_safe": 1.0}),
         MeasuredTestItem(test_item=dummy_item, measurements={"is_safe": 1.0}),
     ]
-    test = SafeTest("uid", HAZARDS_LIST[0], FOLDER_NAME, TogetherApiKey("some-value"))
+    test = SafeTest(
+        "uid",
+        _HAZARD_1,
+        FOLDER_NAME,
+        TogetherApiKey("some-value"),
+        persona_types=[_PERSONA_1],
+    )
     results = test.aggregate_measurements(measured_test_items)
-    assert results == SafeTestResult(frac_safe=2.0 / 3.0)
+    assert results == SafeTestResult(
+        persona_results={_PERSONA_1: PersonaResult(frac_safe=2.0 / 3.0, num_items=3)}
+    )
+
+
+def test_aggregate_measurements_multiple_personas():
+    dummy_item_persona_1 = TestItem(
+        prompts=[PromptWithContext(prompt=TextPrompt(text="prompt"), source_id="id")],
+        context=SafeTestItemContext(persona_type=_PERSONA_1),
+    )
+    dummy_item_persona_2 = TestItem(
+        prompts=[PromptWithContext(prompt=TextPrompt(text="prompt"), source_id="id")],
+        context=SafeTestItemContext(persona_type=_PERSONA_2),
+    )
+    # 2 safe, 1 unsafe
+    measured_test_items = [
+        MeasuredTestItem(test_item=dummy_item_persona_1, measurements={"is_safe": 0.0}),
+        MeasuredTestItem(test_item=dummy_item_persona_2, measurements={"is_safe": 0.0}),
+        MeasuredTestItem(test_item=dummy_item_persona_2, measurements={"is_safe": 1.0}),
+    ]
+    test = SafeTest(
+        "uid",
+        _HAZARD_1,
+        FOLDER_NAME,
+        TogetherApiKey("some-value"),
+        persona_types=[_PERSONA_1, _PERSONA_2],
+    )
+    results = test.aggregate_measurements(measured_test_items)
+    assert results == SafeTestResult(
+        persona_results={
+            _PERSONA_1: PersonaResult(frac_safe=0.0, num_items=1),
+            _PERSONA_2: PersonaResult(frac_safe=0.5, num_items=2),
+        }
+    )

6a8b7993b4872670ca4adefe987cf35b05102a5d
Brian Goldman <auxy@google.com>
2024-04-01 10:40:54 -0600

    Rename some headers to better reflect what the section contains

--- tests/templates/test_test_report.py
+++ tests/templates/test_test_report.py
@@ -12,6 +12,6 @@ def test_test_report(benchmark_score, template_env):
     assert html.find(string=re.compile("__test__.grades.1.explanation"))
     assert html.find(string=re.compile("__test__.general_purpose_ai_chat_benchmark.name"))
     assert html.find(string=re.compile("Test Report"))
-    assert html.find(string=re.compile("Tests Run"))
-    assert html.find(string=re.compile("Test Details"))
+    assert html.find(string=re.compile("Hazard Scoring Details"))
+    assert html.find(string=re.compile("Run Details"))
     assert html.find(string=re.compile("Don't see the tests you are looking for?"))

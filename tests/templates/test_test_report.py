887044280c451e6b5ff899c5ebbc8fdfea8b8043
Mike Lay <mike@mkly.io>
2024-04-12 11:49:20 -0700

    Fix test report test

--- tests/templates/test_test_report.py
+++ tests/templates/test_test_report.py
@@ -11,7 +11,7 @@ def test_test_report(benchmark_score, template_env):
     html = BeautifulSoup(result, "html.parser")
     assert html.find(string=re.compile("__test__.grades.1.explanation"))
     assert html.find(string=re.compile("__test__.general_purpose_ai_chat_benchmark.name"))
-    assert html.find(string=re.compile("Test Report"))
+    assert html.find(string=re.compile("MLCommons AI Safety v0.5 Benchmark Report"))
     assert html.find(string=re.compile("Hazard Scoring Details"))
     assert html.find(string=re.compile("Run Details"))
     assert html.find(string=re.compile("Don't see the tests you are looking for?"))

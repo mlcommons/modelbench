d1cc8ed09a076160b5561c7441c2040443200cd8
william <william@williampietri.com>
2024-08-19 10:45:18 -0500

    Removing commented-out code.

--- src/modelbench/suts.py
+++ src/modelbench/suts.py
@@ -79,28 +79,3 @@ SUTS_FOR_V_0_5 = [
         "wizardlm-13b",
     ]
 ]
-
-# class ModelGaugeSut(SutDescription, Enum):
-#     ALPACA_7B = "alpaca-7b" "qwen-72b"
-#     CHRONOS_HERMES_13B = "chronos-hermes-13b", TogetherChatSUT, "Austism/chronos-hermes-13b"
-#     DEEPSEEK_67B = "deepseek-67b", TogetherChatSUT, "deepseek-ai/deepseek-llm-67b-chat"
-#     GEMMA_7B = "gemma-7b", TogetherChatSUT, "google/gemma-7b-it"
-#     LLAMA_2_7B = "llama-2-7b-chat"
-#     LLAMA_2_13B = "llama-2-13b-chat"
-#     LLAMA_2_70B = "llama-2-70b-chat"
-#     MISTRAL_7B = "mistral-7b", TogetherChatSUT, "mistralai/Mistral-7B-Instruct-v0.2"
-#     MIXTRAL_8X_7B = "mixtral-8x-7b", TogetherChatSUT, "mistralai/Mixtral-8x7B-Instruct-v0.1"
-#     OPENCHAT_3_5 = "openchat-3_5", TogetherChatSUT, "openchat/openchat-3.5-1210"
-#     QWEN_72B = "qwen-72b", TogetherChatSUT, "Qwen/Qwen1.5-72B-Chat"
-#     NOUS = "stripedhyena-nous-7b", TogetherChatSUT, "togethercomputer/StripedHyena-Nous-7B"
-#     VICUNA_13B = "vicuna-13b", TogetherChatSUT, "lmsys/vicuna-13b-v1.5"
-#     WIZARDLM_13B = "wizardlm-13b", TogetherChatSUT, "WizardLM/WizardLM-13B-V1.2"
-#
-#
-# for sut in ModelGaugeSut:
-#     required_secrets = {
-#         TogetherCompletionsSUT: (InjectSecret(TogetherApiKey),),
-#         TogetherChatSUT: (InjectSecret(TogetherApiKey),),
-#     }
-#     if sut.modelgauge_class is not None and sut.key is not None and sut.key not in dict(SUTS.items()):
-#         SUTS.register(sut.modelgauge_class, sut.key, sut.modelgauge_key, *required_secrets[sut.modelgauge_class])

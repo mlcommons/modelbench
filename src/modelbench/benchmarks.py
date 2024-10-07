9065e5c68f06bfec14356e7574f3ef48111d7dbc
Barbara Korycki <barbara@mlcommons.org>
2024-10-07 08:45:23 -0700

    undo accidental commit

--- src/modelbench/benchmarks.py
+++ src/modelbench/benchmarks.py
@@ -109,8 +109,3 @@ class GeneralPurposeAiChatBenchmarkV1(BenchmarkDefinition):
 
     def _make_hazards(self) -> Sequence[HazardDefinition]:
         return [c() for c in SafeHazardV1.__subclasses__()]
-
-
-def make_benchmark(uid):
-    if uid == GeneralPurposeAiChatBenchmark().uid:
-        return GeneralPurposeAiChatBenchmark()

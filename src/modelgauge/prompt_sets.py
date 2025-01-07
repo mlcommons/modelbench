1710e35714c0e957cdc63e15a15559e2475bf8f0
rogthefrog <roger@mlcommons.org>
2025-01-06 20:18:12 -0800

    add version and tests

--- src/modelgauge/prompt_sets.py
+++ src/modelgauge/prompt_sets.py
@@ -17,12 +17,13 @@ class ModellabFileDownloadToken(RequiredSecret):
 class PromptSet(BaseModel):
     prompt_set_type: str
     locale: str
+    version: str
     filename: str
 
     @staticmethod
-    def find(prompt_set_type: str, locale: str, prompt_sets: list) -> "PromptSet | None":
+    def find(prompt_set_type: str, locale: str, version: str, prompt_sets: list) -> "PromptSet | None":
         for ps in prompt_sets:
-            if ps.prompt_set_type == prompt_set_type and ps.locale == locale:
+            if ps.prompt_set_type == prompt_set_type and ps.locale == locale and ps.version == version:
                 return ps
         return None
 
@@ -30,10 +31,23 @@ class PromptSet(BaseModel):
         return f"https://{PROMPT_SET_DOWNLOAD_HOST}/files/download/{self.filename}"
 
 
-PROMPT_SETS = (
-    PromptSet(prompt_set_type="practice", locale="en_us", filename="airr_official_1.0_practice_prompt_set_release.csv"),
-    PromptSet(prompt_set_type="official", locale="en_us", filename="airr_official_1.0_heldback_prompt_set_release.csv"),
+PROMPT_SETS = [
     PromptSet(
-        prompt_set_type="practice", locale="fr_fr", filename="airr_official_1.0_practice_prompt_set_release_fr_fr.csv"
+        prompt_set_type="practice",
+        locale="en_us",
+        version="1.0",
+        filename="airr_official_1.0_practice_prompt_set_release.csv",
     ),
-)
+    PromptSet(
+        prompt_set_type="official",
+        locale="en_us",
+        version="1.0",
+        filename="airr_official_1.0_heldback_prompt_set_release.csv",
+    ),
+    PromptSet(
+        prompt_set_type="practice",
+        locale="fr_fr",
+        version="1.0",
+        filename="airr_official_1.0_practice_prompt_set_release_fr_fr.csv",
+    ),
+]
--- tests/modelgauge_tests/test_prompt_sets.py
+++ tests/modelgauge_tests/test_prompt_sets.py
@@ -1,22 +1,25 @@
-import pytest
-
 from modelgauge.prompt_sets import PROMPT_SETS, PromptSet
 
 
 def test_find():
     prompt_sets = [
-        PromptSet(prompt_set_type="official", locale="hi_hi", filename="some_file1.csv"),
-        PromptSet(prompt_set_type="heldback", locale="en_us", filename="some_file2.csv"),
-        PromptSet(prompt_set_type="official", locale="en_us", filename="some_file3.csv"),
-        PromptSet(prompt_set_type="official", locale="fr_fr", filename="some_file4.csv"),
+        PromptSet(prompt_set_type="official", locale="hi_hi", version="1.0", filename="some_file1.csv"),
+        PromptSet(prompt_set_type="heldback", locale="en_us", version="0.5", filename="some_file2.csv"),
+        PromptSet(prompt_set_type="official", locale="en_us", version="1.0", filename="some_file3.csv"),
+        PromptSet(prompt_set_type="official", locale="fr_fr", version="1.1", filename="some_file4.csv"),
     ]
-    assert PromptSet.find("official", "hi_hi", prompt_sets) == prompt_sets[0]
-    assert PromptSet.find("heldback", "en_us", prompt_sets) == prompt_sets[1]
-    assert PromptSet.find("official", "en_us", prompt_sets) == prompt_sets[2]
-    assert PromptSet.find("official", "fr_fr", prompt_sets) == prompt_sets[3]
-    assert PromptSet.find("fake", "fr_fr", prompt_sets) is None
+    assert PromptSet.find("official", "hi_hi", "1.0", prompt_sets) == prompt_sets[0]
+    assert PromptSet.find("heldback", "en_us", "0.5", prompt_sets) == prompt_sets[1]
+    assert PromptSet.find("official", "en_us", "1.0", prompt_sets) == prompt_sets[2]
+    assert PromptSet.find("official", "fr_fr", "1.1", prompt_sets) == prompt_sets[3]
+    assert PromptSet.find("fake", "fr_fr", "2.0", prompt_sets) is None
+
+    # the blessed PROMPT_SETS must always include en_us official and heldback 1.0
+    assert PromptSet.find("practice", "en_us", "1.0", PROMPT_SETS) is not None
+    assert PromptSet.find("official", "en_us", "1.0", PROMPT_SETS) is not None
+    assert PromptSet.find("practice", "fr_fr", "1.0", PROMPT_SETS) is not None
 
 
 def test_url():
-    ps = PromptSet(prompt_set_type="official", locale="hi_hi", filename="some_file1.csv")
+    ps = PromptSet(prompt_set_type="official", locale="hi_hi", version="1.0", filename="some_file1.csv")
     assert ps.url() == "https://ailuminate.mlcommons.org/files/download/some_file1.csv"

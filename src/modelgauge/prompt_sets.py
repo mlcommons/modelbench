4fea25f30223c1f282576c7c9c27c8af8360f29a
rogthefrog <roger@mlcommons.org>
2025-01-07 19:18:26 -0800

    decouple the semantics of a prompt set from its filename

--- src/modelgauge/prompt_sets.py
+++ src/modelgauge/prompt_sets.py
@@ -1,7 +1,19 @@
+from enum import Enum
+
 from modelgauge.secret_values import RequiredSecret, SecretDescription
 from pydantic import BaseModel
 
 PROMPT_SET_DOWNLOAD_HOST = "ailuminate.mlcommons.org"
+PROMPT_SET_DEFAULT_VERSION = "1.0"
+PROMPT_SET_TYPES = "practice"
+
+
+class PromptSetType(str, Enum):
+    PRACTICE = "practice"
+    OFFICIAL = "official"
+
+    def __str__(self):
+        return str(self.value)
 
 
 class ModellabFileDownloadToken(RequiredSecret):
@@ -16,12 +28,31 @@ class ModellabFileDownloadToken(RequiredSecret):
 
 class PromptSet(BaseModel):
     prompt_set_type: str
-    locale: str
+    locale: str  # may be none, for files containing multiple locales
     version: str
     filename: str
 
     @staticmethod
-    def find(prompt_set_type: str, locale: str, version: str, prompt_sets: list) -> "PromptSet | None":
+    def find(
+        prompt_set: "PromptSet",
+        prompt_sets: list = [],
+    ) -> "PromptSet | None":
+        for ps in prompt_sets:
+            if (
+                ps.prompt_set_type == prompt_set.prompt_set_type
+                and ps.locale == prompt_set.locale
+                and ps.version == prompt_set.version
+            ):
+                return ps
+        return None
+
+    @staticmethod
+    def find_by(
+        prompt_set_type: str,
+        locale: str | None,
+        version: str = PROMPT_SET_DEFAULT_VERSION,
+        prompt_sets: list = [],
+    ) -> "PromptSet | None":
         for ps in prompt_sets:
             if ps.prompt_set_type == prompt_set_type and ps.locale == locale and ps.version == version:
                 return ps
@@ -33,19 +64,19 @@ class PromptSet(BaseModel):
 
 PROMPT_SETS = [
     PromptSet(
-        prompt_set_type="practice",
+        prompt_set_type=PromptSetType.PRACTICE,
         locale="en_us",
         version="1.0",
         filename="airr_official_1.0_practice_prompt_set_release.csv",
     ),
     PromptSet(
-        prompt_set_type="official",
+        prompt_set_type=PromptSetType.OFFICIAL,
         locale="en_us",
         version="1.0",
         filename="airr_official_1.0_heldback_prompt_set_release.csv",
     ),
     PromptSet(
-        prompt_set_type="practice",
+        prompt_set_type=PromptSetType.PRACTICE,
         locale="fr_fr",
         version="1.0",
         filename="airr_official_1.0_practice_prompt_set_release_fr_fr.csv",
--- tests/modelgauge_tests/test_prompt_sets.py
+++ tests/modelgauge_tests/test_prompt_sets.py
@@ -1,23 +1,39 @@
+import pytest
 from modelgauge.prompt_sets import PROMPT_SETS, PromptSet
 
 
-def test_find():
-    prompt_sets = [
+@pytest.fixture()
+def prompt_sets():
+    return [
         PromptSet(prompt_set_type="official", locale="hi_hi", version="1.0", filename="some_file1.csv"),
         PromptSet(prompt_set_type="heldback", locale="en_us", version="0.5", filename="some_file2.csv"),
         PromptSet(prompt_set_type="official", locale="en_us", version="1.0", filename="some_file3.csv"),
         PromptSet(prompt_set_type="official", locale="fr_fr", version="1.1", filename="some_file4.csv"),
     ]
-    assert PromptSet.find("official", "hi_hi", "1.0", prompt_sets) == prompt_sets[0]
-    assert PromptSet.find("heldback", "en_us", "0.5", prompt_sets) == prompt_sets[1]
-    assert PromptSet.find("official", "en_us", "1.0", prompt_sets) == prompt_sets[2]
-    assert PromptSet.find("official", "fr_fr", "1.1", prompt_sets) == prompt_sets[3]
-    assert PromptSet.find("fake", "fr_fr", "2.0", prompt_sets) is None
+
+
+def test_find(prompt_sets):
+    for p in prompt_sets:
+        assert PromptSet.find(p, prompt_sets) is not None
+    assert (
+        PromptSet.find(
+            PromptSet(prompt_set_type="official", locale="zh_cn", version="1.0", filename="some_file1.csv"), prompt_sets
+        )
+        is None
+    )
+
+
+def test_find_by(prompt_sets):
+    assert PromptSet.find_by("official", "hi_hi", "1.0", prompt_sets) == prompt_sets[0]
+    assert PromptSet.find_by("heldback", "en_us", "0.5", prompt_sets) == prompt_sets[1]
+    assert PromptSet.find_by("official", "en_us", "1.0", prompt_sets) == prompt_sets[2]
+    assert PromptSet.find_by("official", "fr_fr", "1.1", prompt_sets) == prompt_sets[3]
+    assert PromptSet.find_by("fake", "fr_fr", "2.0", prompt_sets) is None
 
     # the blessed PROMPT_SETS must always include en_us official and heldback 1.0
-    assert PromptSet.find("practice", "en_us", "1.0", PROMPT_SETS) is not None
-    assert PromptSet.find("official", "en_us", "1.0", PROMPT_SETS) is not None
-    assert PromptSet.find("practice", "fr_fr", "1.0", PROMPT_SETS) is not None
+    assert PromptSet.find_by("practice", "en_us", "1.0", PROMPT_SETS) is not None
+    assert PromptSet.find_by("official", "en_us", "1.0", PROMPT_SETS) is not None
+    assert PromptSet.find_by("practice", "fr_fr", "1.0", PROMPT_SETS) is not None
 
 
 def test_url():

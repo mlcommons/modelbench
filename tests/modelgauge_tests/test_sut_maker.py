82d9bcc0d357f6d241b7339d0ced00f86f96b92e
rogthefrog <roger@mlcommons.org>
2025-04-30 21:08:20 -0400

    PoC of dynamic SUTs

---
+++ tests/modelgauge_tests/test_sut_maker.py
@@ -0,0 +1,16 @@
+import pytest
+from modelgauge.sut import SUTMaker
+
+
+def test_parse_sut_name():
+    with pytest.raises(ValueError):
+        _ = SUTMaker.parse_sut_name("/a/b/c/d/e/f/g")
+
+    assert SUTMaker.parse_sut_name("hf/nebius/google/gemma-7b-it") == ("hf", "nebius", "google", "gemma-7b-it")
+    assert SUTMaker.parse_sut_name("hf/google/gemma-7b-it") == (None, "hf", "google", "gemma-7b-it")
+    assert SUTMaker.parse_sut_name("meta-llama/Llama-3.1-8B-Instruct") == (
+        None,
+        None,
+        "meta-llama",
+        "Llama-3.1-8B-Instruct",
+    )

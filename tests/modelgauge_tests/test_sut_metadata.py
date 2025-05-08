76f550168189c660a626e93c04e256608e06fd42
rogthefrog <roger@mlcommons.org>
2025-05-07 21:13:15 -0400

    intermediate checkpoint for new SUT UID scheme

---
+++ tests/modelgauge_tests/test_sut_metadata.py
@@ -0,0 +1,89 @@
+import random
+
+import pytest
+from modelgauge.sut import SUTMetadata
+
+sut_uids = (
+    "amazon-nova-1.0-lite",
+    "amazon-nova-1.0-micro",
+    "amazon-nova-1.0-pro",
+    "athene-v2-chat",
+    "aya-expanse-8b",
+    "claude-3-5-haiku-20241022",
+    "claude-3-5-sonnet-20241022",
+    "cohere-c4ai-command-a-03-2025",
+    "deepseek-R1",
+    "deepseek-v3",
+    "demo_always_angry",
+    "demo_always_sorry",
+    "demo_random_words",
+    "demo_yes_no",
+    "gemini-1.5-flash",
+    "gemini-1.5-flash-8b",
+    "gemini-1.5-flash-8b-safety_block_most",
+    "gemini-1.5-flash-8b-safety_block_none",
+    "gemini-1.5-flash-safety_block_most",
+    "gemini-1.5-flash-safety_block_none",
+    "gemini-1.5-pro",
+    "gemini-1.5-pro-safety_block_most",
+    "gemini-1.5-pro-safety_block_none",
+    "gemini-2.0-flash",
+    "gemini-2.0-flash-lite",
+    "gemini-2.0-flash-lite-safety_block_most",
+    "gemini-2.0-flash-lite-safety_block_none",
+    "gemini-2.0-flash-safety_block_most",
+    "gemini-2.0-flash-safety_block_none",
+    "gemma-2-9b-it",
+    "google-gemma-3-27b-it-hf-nebius",
+    "gpt-3.5-turbo",
+    "gpt-4o",
+    "gpt-4o-mini",
+    "llama-3-1-tulu-3-70b",
+    "llama-3-1-tulu-3-8b",
+    "llama-3-70b-chat",
+    "llama-3-70b-chat",
+    "llama-3.1-405b-instruct-turbo",
+    "llama-3.1-8b-instruct-turbo",
+    "llama-3.3-70b-instruct-turbo",
+    "meta-llama-3_1-8b-instruct-hf-nebius",
+    "Mistral-7B-Instruct-v0.2",
+    "mistral-8x22b-instruct",
+    "mistral-nemo-instruct-2407",
+    "mistralai-ministral-8b-2410",
+    "mistralai-ministral-8b-2410-moderated",
+    "mistralai-mistral-large-2411",
+    "mistralai-mistral-large-2411-moderated",
+    "Mixtral-8x7B-Instruct-v0.1",
+    "nvidia-llama-3-1-nemotron-nano-8b-v1",
+    "nvidia-llama-3.1-nemotron-70b-instruct",
+    "nvidia-llama-3.3-49b-nemotron-super",
+    "nvidia-mistral-nemo-minitron-8b-8k-instruct",
+    "nvidia-nemotron-4-340b-instruct",
+    "nvidia-nemotron-mini-4b-instruct",
+    "olmo-2-0325-32b-instruct",
+    "olmo-2-1124-7b-instruct",
+    "olmo-7b-0724-instruct",
+    "phi-3.5-mini-instruct",
+    "phi-3.5-moe-instruct",
+    "phi-4",
+    "qwen2-5-7b-instruct",
+    "qwen2.5-7B-instruct-turbo",
+    "qwq-32b",
+    "vertexai-mistral-large-2411",
+    "yi-1-5-34b-chat",
+)
+
+
+@pytest.mark.parametrize("model", sut_uids)
+def test_good_models(model):
+    assert SUTMetadata(model=model, vendor="meta", provider="hf")
+
+
+@pytest.mark.parametrize("model", sut_uids)
+def test_bad_models(model):
+    bad = list(r'!@#$%^&*():<?,+=`|\]{}[" \'')
+    head = random.choice(bad)
+    tail = random.choice(bad)
+    model = f"{head}{model}{tail}"
+    with pytest.raises(ValueError):
+        _ = SUTMetadata(model=model, vendor="vendor", provider="provider", driver="driver", date="20250101")

dcac74406bfd029b2902b86a706724b96947914d
Barbara Korycki <barbara@mlcommons.org>
2025-06-18 13:43:58 -0700

    Data schema objects

---
+++ src/modelgauge/data_schema.py
@@ -0,0 +1,94 @@
+ANNOTATOR_CSV_INPUT_COLUMNS = ["UID", "Prompt", "SUT", "Response"]
+PROMPT_CSV_INPUT_COLUMNS = {
+    "default": {"id": "UID", "text": "Text"},
+    "prompt_set": {"id": "release_prompt_id", "text": "prompt_text"},  # official prompt set files
+    "db": {"id": "prompt_uid", "text": "prompt_text"},  # database dumps
+}
+
+# The first value is the preferred name.
+PROMPT_UID_COLS = ["prompt_uid", "release_prompt_id"]
+PROMPT_TEXT_COLS = ["prompt_text"]
+SUT_UID_COLS = ["sut_uid", "sut"]
+SUT_RESPONSE_COLS = ["sut_response", "response_text", "response"]
+
+
+class SchemaValidationError(ValueError):
+    """Exception raised when schema validation fails."""
+
+    def __init__(self, missing_columns):
+        """missing_columns: a list where each element is a string or a list of strings. List elements are used to indicate that the column can be one of several options."""
+        self.missing_columns = missing_columns
+        super().__init__(str(self))
+
+    def __str__(self):
+        message = "Missing required columns:"
+        for column in self.missing_columns:
+            if isinstance(column, str):
+                message += f"\n\t{column}"
+            elif len(column) == 1:
+                message += f"\n\t{column[0]}"
+            else:
+                message += f"\n\tone of: {column}"
+        return message
+
+
+class PromptSchema:
+    """A case-insensitive schema for a prompts file.
+
+    Attributes:
+        prompt_uid: The column name for the prompt uid.
+        prompt_text: The column name for the prompt text.
+    """
+
+    def __init__(self, header: list[str]):
+        self.prompt_uid = self._find_column(header, PROMPT_UID_COLS)
+        self.prompt_text = self._find_column(header, PROMPT_TEXT_COLS)
+        self._validate()
+
+    def _find_column(self, header, columns):
+        return next((col for col in header if col.lower() in columns), None)
+
+    def _validate(self):
+        """Validates that all required columns were found in the header.
+
+        Raises:
+            SchemaValidationError: If any required columns are missing.
+        """
+        missing = []
+        if self.prompt_uid is None:
+            missing.append(PROMPT_UID_COLS)
+        if self.prompt_text is None:
+            missing.append(PROMPT_TEXT_COLS)
+
+        if missing:
+            raise SchemaValidationError(missing)
+
+
+class PromptResponseSchema(PromptSchema):
+    """A schema for a prompt + response file that is used as annotation input.
+    Attributes:
+        prompt_uid: The column name for the prompt uid. (same as PromptSchema)
+        prompt_text: The column name for the prompt text. (same as PromptSchema)
+        sut_uid: The column name for the SUT uid.
+        sut_response: The column name for the SUT response.
+    """
+
+    def __init__(self, header: list[str]):
+        self.sut_uid = self._find_column(header, SUT_UID_COLS)
+        self.sut_response = self._find_column(header, SUT_RESPONSE_COLS)
+        super().__init__(header)  # Iniitalize the prompt schema columns and then validate.
+
+    def _validate(self):
+        missing = []
+        # Validate that the prompt schema is valid
+        try:
+            super()._validate()
+        except SchemaValidationError as e:
+            missing.extend(e.missing_columns)
+        # Validate that the SUT uid and response columns are present
+        if self.sut_uid is None:
+            missing.append(SUT_UID_COLS)
+        if self.sut_response is None:
+            missing.append(SUT_RESPONSE_COLS)
+        if missing:
+            raise SchemaValidationError(missing)
---
+++ tests/modelgauge_tests/test_data_schema.py
@@ -0,0 +1,73 @@
+import pytest
+
+from modelgauge.data_schema import (
+    PROMPT_TEXT_COLS,
+    PROMPT_UID_COLS,
+    PromptResponseSchema,
+    PromptSchema,
+    SchemaValidationError,
+)
+
+
+def test_schema_validation_error():
+    error = SchemaValidationError(["one", "two"])
+    assert error.missing_columns == ["one", "two"]
+    assert str(error) == "Missing required columns:\n\tone\n\ttwo"
+
+
+def test_schema_validation_error_multiple_options():
+    error = SchemaValidationError([["one", "a"], "two"])
+    assert error.missing_columns == [["one", "a"], "two"]
+    assert str(error) == "Missing required columns:\n\tone of: ['one', 'a']\n\ttwo"
+
+
+@pytest.mark.parametrize(
+    "header",
+    [
+        ["prompt_uid", "prompt_text"],  # Preferred names.
+        ["Prompt_UID", "Prompt_Text"],  # Case-insensitive
+        ["release_prompt_id", "prompt_text"],
+        ["release_prompt_id", "prompt_text", "random_column"],  # Extra columns are allowed.
+    ],
+)
+def test_valid_prompt_schema(header):
+    schema = PromptSchema(header)
+    assert schema.prompt_uid == header[0]
+    assert schema.prompt_text == header[1]
+
+
+def test_invalid_prompt_schema():
+    header = ["random_column", "random_column_2"]
+    with pytest.raises(SchemaValidationError) as e:
+        schema = PromptSchema(header)
+        assert set(e.missing_columns) == {PROMPT_UID_COLS, PROMPT_TEXT_COLS}
+
+
+@pytest.mark.parametrize(
+    "header",
+    [
+        ["prompt_uid", "prompt_text", "sut_uid", "sut_response"],  # Preferred names.
+        ["prompt_UID", "Prompt_Text", "SUT_UID", "SUT_Response"],  # Case-insensitive
+        ["release_prompt_id", "prompt_text", "sut", "response"],
+    ],
+)
+def test_valid_prompt_response_schema(header):
+    schema = PromptResponseSchema(header)
+    assert schema.prompt_uid == header[0]
+    assert schema.prompt_text == header[1]
+    assert schema.sut_uid == header[2]
+    assert schema.sut_response == header[3]
+
+
+def test_valid_prompt_invalid_response_schema():
+    header = ["prompt_uid", "prompt_text", "random_column", "random_column_2"]
+    with pytest.raises(SchemaValidationError) as e:
+        schema = PromptResponseSchema(header)
+        assert set(e.missing_columns) == {SUT_UID_COLS, SUT_RESPONSE_COLS}
+
+
+def test_invalid_prompt_valid_response_schema():
+    header = ["random_column", "random_column_2", "prompt_uid", "prompt_text"]
+    with pytest.raises(SchemaValidationError) as e:
+        schema = PromptResponseSchema(header)
+        assert set(e.missing_columns) == {PROMPT_UID_COLS, PROMPT_TEXT_COLS}

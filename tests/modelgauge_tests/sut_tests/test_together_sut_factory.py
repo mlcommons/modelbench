51bbc9b47fc95e00f9da46673bf265646864dcff
Barbara Korycki <bkorycki@gmail.com>
2025-07-16 15:16:59 -0400

    fix tests (#1134)

--- tests/modelgauge_tests/sut_tests/test_together_sut_factory.py
+++ tests/modelgauge_tests/sut_tests/test_together_sut_factory.py
@@ -10,7 +10,7 @@ from modelgauge.suts.together_sut_factory import TogetherSUTFactory
 
 
 def test_make_sut():
-    with patch("modelgauge.suts.together_sut_factory.TogetherSUTFactory.find", return_value="google/gemma:together"):
+    with patch("modelgauge.suts.together_sut_factory.TogetherSUTFactory._find", return_value="google/gemma:together"):
         sut_metadata = DynamicSUTMetadata(model="gemma", maker="google", driver="together")
         found_sut = TogetherSUTFactory.make_sut(sut_metadata)
         assert found_sut is not None
@@ -21,7 +21,7 @@ def test_make_sut_bad_model():
     os.environ["TOGETHER_API_KEY"] = "fake-key"
 
     sut_metadata = DynamicSUTMetadata(model="bogus", maker="fake", driver="together")
-    with patch("modelgauge.suts.together_sut_factory.TogetherSUTFactory.find", side_effect=ModelNotSupportedError()):
+    with patch("modelgauge.suts.together_sut_factory.TogetherSUTFactory._find", side_effect=ModelNotSupportedError()):
         with pytest.raises(ModelNotSupportedError):
             _ = TogetherSUTFactory.make_sut(sut_metadata)
 
@@ -34,7 +34,7 @@ def test_find():
         return_value=[{"id": "google/gemma"}],
     ):
         sut_metadata = DynamicSUTMetadata(model="gemma", maker="google", driver="together")
-        assert TogetherSUTFactory.find(sut_metadata) == sut_metadata.external_model_name()
+        assert TogetherSUTFactory._find(sut_metadata) == sut_metadata.external_model_name()
 
 
 def test_find_bad_model():
@@ -44,4 +44,4 @@ def test_find_bad_model():
         return_value=None,
     ):
         with pytest.raises(ModelNotSupportedError):
-            _ = TogetherSUTFactory.find(sut_metadata)
+            _ = TogetherSUTFactory._find(sut_metadata)

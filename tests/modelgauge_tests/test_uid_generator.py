24cda62d81ae8252d1ccc383374bec6b3e7c7b8d
rogthefrog <roger@mlcommons.org>
2025-07-24 17:09:26 -0400

    add tests, minor refactors

--- tests/modelgauge_tests/test_uid_generator.py
+++ tests/modelgauge_tests/test_uid_generator.py
@@ -1,3 +1,4 @@
+from modelgauge.dynamic_sut_metadata import DynamicSUTMetadata
 from modelgauge.uid_generator import SUTUIDGenerator
 
 
@@ -20,3 +21,27 @@ def test_uid():
 
     g.add("display_name", "my favorite SUT")
     assert g.uid == "openai/chatgpt-4o:openai:openai:20250723_dv:abcde_mod:y_reas:n_t:0.8_p:6_k:5_dn:my-favorite-sut"
+
+
+def test_from_json():
+    data = {"model": "the_model", "driver": "the_driver"}
+    g = SUTUIDGenerator.from_json(data)
+    assert g.data["model"] == "the_model"
+    assert g.data["driver"] == "the_driver"
+
+    data_s = '{"model": "my_model", "driver": "my_driver"}'
+    gg = SUTUIDGenerator.from_json(data_s)
+    assert gg.data["model"] == "my_model"
+    assert gg.data["driver"] == "my_driver"
+
+
+def test_to_dynamic_sut_metadata():
+    data = {
+        "model": "the_model",
+        "driver": "the_driver",
+        "maker": "the_maker",
+        "provider": "the_provider",
+        "date": "20250724",
+    }
+    g = SUTUIDGenerator(data)
+    assert g.to_dynamic_sut_metadata() == DynamicSUTMetadata(**data)

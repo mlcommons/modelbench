327150d4c2fd02dbab741ce856bbed899ea68856
rogthefrog <roger@mlcommons.org>
2025-07-25 15:50:13 -0400

    fix name of temperature argument and weird unit test bug

--- tests/modelgauge_tests/test_uid_generator.py
+++ tests/modelgauge_tests/test_uid_generator.py
@@ -7,17 +7,21 @@ def test_uid():
     d.add("maker", "openai")
     d.add("driver", "openai")
     d.add("provider", "openai")
-    d.add("temperature", 0.8)
+    d.add("date", None)  # workaround for the test in test_to_dynamic_sut_metadata setting the date (?!)
+    d.add("temp", 0.8)
     d.add("top_p", 6)
     d.add("top_k", 5)
+    d.add("top_logprobs", 1)
     d.add("moderated", True)
     d.add("reasoning", False)
     g = SUTUIDGenerator(d)
-    assert g.uid == "openai/chatgpt-4o:openai:openai_mod:y_reas:n_t:0.8_p:6_k:5"
+    assert g.uid == "openai/chatgpt-4o:openai:openai_mod:y_reas:n_t:0.8_p:6_k:5_l:1"
 
     d.add("driver_code_version", "abcde")
     d.add("date", "20250723")
-    assert g.uid == "openai/chatgpt-4o:openai:openai:20250723_dv:abcde_mod:y_reas:n_t:0.8_p:6_k:5"
+    assert g.uid == "openai/chatgpt-4o:openai:openai:20250723_dv:abcde_mod:y_reas:n_t:0.8_p:6_k:5_l:1"
 
     d.add("display_name", "my favorite SUT")
-    assert g.uid == "openai/chatgpt-4o:openai:openai:20250723_dv:abcde_mod:y_reas:n_t:0.8_p:6_k:5_dn:my-favorite-sut"
+    assert (
+        g.uid == "openai/chatgpt-4o:openai:openai:20250723_dv:abcde_mod:y_reas:n_t:0.8_p:6_k:5_l:1_dn:my-favorite-sut"
+    )

15eb62d29a13d4749ee8aa49021e89208b2016a4
rogthefrog <roger@mlcommons.org>
2025-07-25 18:11:17 -0400

    fix test

--- tests/modelgauge_tests/test_uid_generator.py
+++ tests/modelgauge_tests/test_uid_generator.py
@@ -8,6 +8,7 @@ def test_uid():
     d.add("driver", "openai")
     d.add("provider", "openai")
     d.add("date", None)  # workaround for the test in test_to_dynamic_sut_metadata setting the date (?!)
+    d.add("max_tokens", 500)
     d.add("temp", 0.8)
     d.add("top_p", 6)
     d.add("top_k", 5)
@@ -15,13 +16,14 @@ def test_uid():
     d.add("moderated", True)
     d.add("reasoning", False)
     g = SUTUIDGenerator(d)
-    assert g.uid == "openai/chatgpt-4o:openai:openai_mod:y_reas:n_t:0.8_p:6_k:5_l:1"
+    assert g.uid == "openai/chatgpt-4o:openai:openai;mod:y;reas:n;mt:500;t:0.8;p:6;k:5;l:1"
 
     d.add("driver_code_version", "abcde")
     d.add("date", "20250723")
-    assert g.uid == "openai/chatgpt-4o:openai:openai:20250723_dv:abcde_mod:y_reas:n_t:0.8_p:6_k:5_l:1"
+    assert g.uid == "openai/chatgpt-4o:openai:openai:20250723;dv:abcde;mod:y;reas:n;mt:500;t:0.8;p:6;k:5;l:1"
 
     d.add("display_name", "my favorite SUT")
     assert (
-        g.uid == "openai/chatgpt-4o:openai:openai:20250723_dv:abcde_mod:y_reas:n_t:0.8_p:6_k:5_l:1_dn:my-favorite-sut"
+        g.uid
+        == "openai/chatgpt-4o:openai:openai:20250723;dv:abcde;mod:y;reas:n;mt:500;t:0.8;p:6;k:5;l:1;dn:my_favorite_sut"
     )

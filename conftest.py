5e5c889e05a8fc5270369529e47e888c4feaf6cc
dhosterman <daniel@danielhosterman.com>
2025-08-26 10:54:34 -0400

    attempting to remove the need for any config files or workarounds from testing

--- conftest.py
+++ conftest.py
@@ -1,7 +1,10 @@
+import importlib
+import sys
 from datetime import datetime, timedelta, timezone
 from typing import Dict
 
 import pytest
+
 from modelgauge.secret_values import (
     get_all_secrets,
 )
@@ -41,3 +44,27 @@ def pytest_addoption(parser):
         dest="expensive-tests",
         help="enable expensive tests",
     )
+
+
+from collections import defaultdict
+import copy
+
+
+@pytest.hookimpl(tryfirst=True)
+def pytest_sessionstart(session):
+    import modelgauge.config as mg_config
+
+    mock_secret = defaultdict(lambda: defaultdict(lambda: "fake-secret"))
+    mock_secret["demo"] = {"api_key": "12345"}
+
+    original_func = copy.copy(mg_config.load_secrets_from_config)
+
+    def new_func(path=None):
+        if not path:
+            return mock_secret
+        else:
+            return original_func(path)
+
+    mg_config.load_secrets_from_config = new_func
+    if "modelgauge.sut_factory" in sys.modules:
+        importlib.reload(sys.modules["modelgauge.sut_factory"])

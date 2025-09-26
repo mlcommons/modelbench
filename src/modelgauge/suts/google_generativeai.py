729d29c3c4a911b1b748217d47f522cbba12d132
rogthefrog <roger@mlcommons.org>
2025-09-26 14:52:51 -0400

    remove all the gemini-1.5 SUTs

--- src/modelgauge/suts/google_generativeai.py
+++ src/modelgauge/suts/google_generativeai.py
@@ -205,8 +205,6 @@ class GoogleGenerativeAiSafetyOnSUT(GoogleGenerativeAiBaseSUT):
 
 
 gemini_models = [
-    "gemini-1.5-flash-8b",
-    "gemini-1.5-pro",
     "gemini-2.0-flash",
     "gemini-2.0-flash-lite",
     "gemini-2.0-flash-001",

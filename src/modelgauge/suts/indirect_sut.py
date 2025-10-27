2e180db3eabdc04817847125dad4f342c3434afa
Barbara Korycki <bkorycki@gmail.com>
2025-10-27 19:48:13 -0400

    Baas sut (#1364)

---
+++ src/modelgauge/suts/indirect_sut.py
@@ -0,0 +1,145 @@
+import threading
+from queue import Queue
+
+import fastapi
+import uvicorn
+from pydantic import BaseModel
+
+from modelgauge.dynamic_sut_factory import DynamicSUTFactory
+from modelgauge.prompt import TextPrompt
+from modelgauge.ready import ReadyResponse
+from modelgauge.secret_values import InjectSecret
+from modelgauge.sut import PromptResponseSUT, SUTResponse, SUTOptions
+from modelgauge.suts.openai_client import _USER_ROLE as USER_ROLE, OpenAIChatRequest, OpenAIChatMessage
+from modelgauge.sut_capabilities import AcceptsTextPrompt
+from modelgauge.sut_decorator import modelgauge_sut
+from modelgauge.sut_definition import SUTDefinition
+
+
+class ThreadsafeIdGenerator:
+
+    def __init__(self):
+        super().__init__()
+        self._lock = threading.Lock()
+        self._last_id = 0
+
+    def next(self):
+        with self._lock:
+            self._last_id += 1
+            return self._last_id
+
+
+class IndirectSUTRequest(OpenAIChatRequest):
+    request_id: int
+
+
+class IndirectSUTResponse(BaseModel):
+    request_id: int
+    response: str
+
+
+@modelgauge_sut(capabilities=[AcceptsTextPrompt])
+class IndirectSUT(PromptResponseSUT):
+
+    def __init__(self, uid: str, model_name: str):
+        super().__init__(uid)
+        self._model_name = model_name
+        self._server = IndirectSUTServer(7777)
+        self._id_generator = ThreadsafeIdGenerator()
+
+    def is_ready(self) -> ReadyResponse:
+        return ReadyResponse(True)
+
+    def translate_text_prompt(self, prompt: TextPrompt, options: SUTOptions) -> IndirectSUTRequest:
+        messages = [OpenAIChatMessage(content=prompt.text, role=USER_ROLE)]
+        return IndirectSUTRequest(
+            request_id=self._id_generator.next(),
+            model=self._model_name,
+            messages=messages,
+            max_completion_tokens=options.max_tokens,
+            temperature=options.temperature,
+        )
+
+    def evaluate(self, request: IndirectSUTRequest) -> IndirectSUTResponse:
+        return self._server.get_response(request)
+
+    def translate_response(self, request: IndirectSUTRequest, response: IndirectSUTResponse) -> SUTResponse:
+        return SUTResponse(text=response.response)
+
+    def run(self):
+        self._server.run()
+
+
+class IndirectSUTServer:
+    def __init__(self, port: int):
+        self.port = port
+        self.prompts = []
+        self.queues: dict[int, Queue] = {}
+        self.outstanding_requests: dict[int, IndirectSUTRequest] = {}
+        self.completed_requests: list[int] = []
+        self.app = self.make_app()
+
+    def make_app(self):
+        app = fastapi.FastAPI()
+
+        @app.get("/debug")
+        def debug_info():
+            return {
+                "outstanding_requests": self.outstanding_requests,
+                "queues": {n: str(q) for n, q in self.queues.items()},
+            }
+
+        @app.get("/requests", response_model_exclude_none=True)
+        def get_requests() -> list[IndirectSUTRequest]:
+            return list(self.outstanding_requests.values())
+
+        @app.post("/responses")
+        def post_responses(responses: list[IndirectSUTResponse]):
+            bad_ids = []
+            for response in responses:
+                try:
+                    queue = self.queues[response.request_id]
+                    queue.put(response)
+                except KeyError:
+                    if response.request_id not in self.completed_requests:
+                        bad_ids.append(response.request_id)
+                    # else: Already completed; ignore.
+                    continue
+            if len(bad_ids):
+                raise fastapi.HTTPException(
+                    status_code=207,
+                    detail=f"Could not find request IDs: {bad_ids}. Ignoring responses for those requests.",
+                )
+
+        return app
+
+    def get_response(self, request: IndirectSUTRequest) -> IndirectSUTResponse:
+        request_id = request.request_id
+        self.outstanding_requests[request_id] = request
+        my_queue = Queue()
+        self.queues[request_id] = my_queue
+        response = my_queue.get()
+        del self.outstanding_requests[request_id]
+        del self.queues[request_id]
+        self.completed_requests.append(request_id)
+        return response
+
+    def run(self):
+
+        def start():
+            uvicorn.run(self.app, host="0.0.0.0", port=self.port, use_colors=False)
+
+        thread = threading.Thread(target=start, daemon=True)
+        thread.start()
+
+
+class IndirectSUTFactory(DynamicSUTFactory):
+
+    def get_secrets(self) -> list[InjectSecret]:
+        return []
+
+    def make_sut(self, sut_definition: SUTDefinition) -> IndirectSUT:
+        sut = IndirectSUT(sut_definition.uid, model_name=sut_definition.external_model_name())
+        sut.run()
+
+        return sut
---
+++ tests/modelgauge_tests/sut_tests/test_indirect_sut.py
@@ -0,0 +1,142 @@
+import pytest
+from fastapi.testclient import TestClient
+from queue import Queue
+from unittest.mock import patch, MagicMock
+
+
+from modelgauge.prompt import TextPrompt
+from modelgauge.sut import SUTOptions
+from modelgauge.suts.indirect_sut import (
+    IndirectSUT,
+    IndirectSUTRequest,
+    IndirectSUTResponse,
+    IndirectSUTServer,
+    USER_ROLE,
+)
+from modelgauge.suts.openai_client import OpenAIChatMessage
+
+
+@pytest.fixture
+def sut_request():
+    messages = [OpenAIChatMessage(content="text", role=USER_ROLE)]
+    return IndirectSUTRequest(request_id=1, messages=messages, model="model-name")
+
+
+class TestSUT:
+    @pytest.fixture
+    def sut(self):
+        fake_server = MagicMock()
+        fake_server.get_response.return_value = IndirectSUTResponse(request_id=1, response="ok")
+
+        with patch("modelgauge.suts.indirect_sut.IndirectSUTServer", return_value=fake_server):
+            return IndirectSUT(uid="fake-sut", model_name="model-name")
+
+    def test_translate_text_prompt(self, sut):
+        prompt = TextPrompt(text="text")
+        options = SUTOptions(max_tokens=20, temperature=0.3)
+
+        request = sut.translate_text_prompt(prompt, options)
+
+        assert request == IndirectSUTRequest(
+            request_id=1,
+            model="model-name",
+            messages=[OpenAIChatMessage(content="text", role=USER_ROLE)],
+            max_completion_tokens=20,
+            temperature=0.3,
+        )
+
+        request = sut.translate_text_prompt(prompt, options)
+        assert request.request_id == 2
+
+    def test_evaluate(self, sut, sut_request):
+        response = sut.evaluate(sut_request)
+
+        assert isinstance(response, IndirectSUTResponse)
+        assert response.request_id == 1
+        assert response.response == "ok"
+
+    def test_translate_response(self, sut, sut_request):
+        response = IndirectSUTResponse(request_id=1, response="ok")
+
+        sut_response = sut.translate_response(sut_request, response)
+
+        assert sut_response.text == "ok"
+
+
+class TestServer:
+    @pytest.fixture
+    def server(self):
+        return IndirectSUTServer(8000)
+
+    @pytest.fixture
+    def client(self, server):
+        return TestClient(server.app)
+
+    def test_requests(self, client, server, sut_request):
+        client_response = client.get("/requests")
+        assert client_response.status_code == 200
+        assert client_response.json() == []
+
+        server.outstanding_requests[sut_request.request_id] = sut_request
+        for _ in range(2):
+            # Make sure we keep the request in the outstanding requests
+            client_response = client.get("/requests")
+            assert client_response.status_code == 200
+            assert client_response.json() == [
+                {
+                    "request_id": 1,
+                    "model": "model-name",
+                    "messages": [{"content": "text", "role": "user"}],
+                }
+            ]
+
+    def test_responses(self, client, server, sut_request):
+        response_data = [{"request_id": 1, "response": "ok"}]
+        server.queues[1] = Queue()
+
+        client_response = client.post("/responses", json=response_data)
+
+        assert client_response.status_code == 200
+        assert server.queues[1].get() == IndirectSUTResponse(request_id=1, response="ok")
+
+    def test_responses_bad_request_id_error_response(self, client, server, sut_request):
+        response_data = [{"request_id": 42, "response": "boo"}]
+        client_response = client.post("/responses", json=response_data)
+        assert client_response.status_code == 207
+        assert server.queues.get(42) is None
+
+    def test_responses_not_all_bad_requests(self, client, server, sut_request):
+        response_data = [
+            {"request_id": 42, "response": "boo"},
+            {"request_id": 1, "response": "ok"},
+        ]  # First one bad, second one good.
+        server.queues[1] = Queue()
+        client_response = client.post("/responses", json=response_data)
+        assert client_response.status_code == 207
+        assert server.queues.get(42) is None
+        assert server.queues[1].qsize() == 1
+        assert server.queues[1].get() == IndirectSUTResponse(request_id=1, response="ok")
+
+    def test_responses_completed_request_id_ignores(self, client, server, sut_request):
+        response_data = [{"request_id": 1, "response": "ok"}]
+        server.completed_requests.append(1)
+        client_response = client.post("/responses", json=response_data)
+        assert client_response.status_code == 200
+        assert server.queues.get(1) is None
+
+    def test_get_response(self, client, server, sut_request):
+        # TODO: Add more edge cases?
+        fake_response = IndirectSUTResponse(request_id=1, response="ok")
+        fake_queue = MagicMock()
+        fake_queue.get.return_value = fake_response
+
+        with patch("modelgauge.suts.indirect_sut.Queue", return_value=fake_queue):
+            response = server.get_response(sut_request)
+
+        # Make sure it was removed from the outstanding requests
+        assert len(server.outstanding_requests) == 0
+        assert server.completed_requests == [1]
+
+        assert isinstance(response, IndirectSUTResponse)
+        assert response.request_id == 1
+        assert response.response == "ok"

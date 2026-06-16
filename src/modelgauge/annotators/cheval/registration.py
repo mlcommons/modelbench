d87b8c1f753980b1f24b5ba50e9beb47322fdcbf
Vishal Doshi <vishal@mlcommons.org>
2026-06-16 15:29:09 -0400

    Add registration for security v1.0 benchmark.

--- src/modelgauge/annotators/cheval/registration.py
+++ src/modelgauge/annotators/cheval/registration.py
@@ -1,6 +1,14 @@
 from modelgauge.annotator_registry import ANNOTATORS
-from modelgauge.annotators.cheval.annotator import ChevalAPIKey, ChevalAnnotator, ChevalEndpointUrl
-from modelgauge.annotators.cheval.ids import SAFETY_ANNOTATOR_V1_1_UID, SECURITY_ANNOTATOR_V0_5_UID
+from modelgauge.annotators.cheval.annotator import (
+    ChevalAnnotator,
+    ChevalAPIKey,
+    ChevalEndpointUrl,
+)
+from modelgauge.annotators.cheval.ids import (
+    SAFETY_ANNOTATOR_V1_1_UID,
+    SECURITY_ANNOTATOR_V0_5_UID,
+    SECURITY_ANNOTATOR_V1_0_UID,
+)
 from modelgauge.secret_values import InjectSecret
 
 ANNOTATORS.register(
@@ -14,6 +22,13 @@ ANNOTATORS.register(
 ANNOTATORS.register(
     ChevalAnnotator,
     SECURITY_ANNOTATOR_V0_5_UID,
+    InjectSecret(ChevalAPIKey),
+    InjectSecret(ChevalEndpointUrl),
+)
+
+ANNOTATORS.register(
+    ChevalAnnotator,
+    SECURITY_ANNOTATOR_V1_0_UID,
     InjectSecret(ChevalAPIKey),
     InjectSecret(ChevalEndpointUrl),
 )

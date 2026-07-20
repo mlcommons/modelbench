70aa342c57c198264a536939109c98982019420e
Barbara Korycki <barbara@mlcommons.org>
2026-07-20 16:48:42 -0700

    rev security benchmark to v1.0.2

--- src/modelgauge/annotators/cheval/registration.py
+++ src/modelgauge/annotators/cheval/registration.py
@@ -1,6 +1,6 @@
 from modelgauge.annotator_registry import ANNOTATORS
 from modelgauge.annotators.cheval.annotator import ChevalAPIKey, ChevalAnnotator, ChevalEndpointUrl
-from modelgauge.annotators.cheval.ids import SAFETY_ANNOTATOR_V1_1_UID, SECURITY_ANNOTATOR_V1_0_1_UID
+from modelgauge.annotators.cheval.ids import SAFETY_ANNOTATOR_V1_1_UID, SECURITY_ANNOTATOR_V1_0_2_UID
 from modelgauge.secret_values import InjectSecret
 
 ANNOTATORS.register(
@@ -13,7 +13,7 @@ ANNOTATORS.register(
 
 ANNOTATORS.register(
     ChevalAnnotator,
-    SECURITY_ANNOTATOR_V1_0_1_UID,
+    SECURITY_ANNOTATOR_V1_0_2_UID,
     InjectSecret(ChevalAPIKey),
     InjectSecret(ChevalEndpointUrl),
 )

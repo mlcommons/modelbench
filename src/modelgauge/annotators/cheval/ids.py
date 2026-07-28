0d4bda56fa07cc2826bad742fdbb0622f15458fb
dhosterman <daniel@danielhosterman.com>
2026-07-28 10:59:39 -0400

    Fix version and use benchmark VERSION attr

--- src/modelgauge/annotators/cheval/ids.py
+++ src/modelgauge/annotators/cheval/ids.py
@@ -1,4 +1,4 @@
-SAFETY_ANNOTATOR_V1_1_UID = "safety-v1.1"
-SECURITY_ANNOTATOR_V0_5_UID = "security-v0.5"
-SECURITY_ANNOTATOR_V1_0_1_UID = "security-v1.0.1"
-SECURITY_ANNOTATOR_V1_0_2_UID = f"security-v.0.2"
+from modelbench.benchmarks import GeneralPurposeAiChatBenchmarkV1_1, SecurityBenchmarkV1_0_2
+
+SAFETY_ANNOTATOR_V1_1_UID = f"safety-v{GeneralPurposeAiChatBenchmarkV1_1.VERSION}"
+SECURITY_ANNOTATOR_V1_0_2_UID = f"security-v{SecurityBenchmarkV1_0_2.VERSION}"

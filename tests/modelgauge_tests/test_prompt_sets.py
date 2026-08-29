6edd26d4d3a6d8efedc19af99041a2130ab6f549
rogthefrog <rogthefrog@gmail.com>
2026-08-28 22:48:17 -0400

    noop; black

--- tests/modelgauge_tests/test_prompt_sets.py
+++ tests/modelgauge_tests/test_prompt_sets.py
@@ -45,8 +45,7 @@ def test_file_base_name():
         == "airr_official_1.2_practice_prompt_set_release_with_visibility"
     )
     assert (
-        prompt_set_file_base_name(SAFETY_1_2_PROMPT_SETS, "official")
-        == "airr_official_1.2_heldback_prompt_set_release"
+        prompt_set_file_base_name(SAFETY_1_2_PROMPT_SETS, "official") == "airr_official_1.2_heldback_prompt_set_release"
     )
     assert (
         prompt_set_file_base_name(SAFETY_1_2_PROMPT_SETS, "demo")
@@ -72,9 +71,7 @@ def test_file_base_name():
         prompt_set_file_base_name({"fake": "thing"}, "practice", "en_us")
 
 
-@pytest.mark.parametrize(
-    "prompt_sets", [GENERAL_PROMPT_SETS, SECURITY_JAILBREAK_PROMPT_SETS, SAFETY_1_2_PROMPT_SETS]
-)
+@pytest.mark.parametrize("prompt_sets", [GENERAL_PROMPT_SETS, SECURITY_JAILBREAK_PROMPT_SETS, SAFETY_1_2_PROMPT_SETS])
 def test_validate_prompt_set(prompt_sets):
     for s in prompt_sets.keys():
         assert validate_prompt_set(prompt_sets, s, "en_us")
